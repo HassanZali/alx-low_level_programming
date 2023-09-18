4e2e036d3de026f45f274546167fb2235e7ac438
hassanzali <hassanzali622@gmail.com>
2023-09-18 14:26:46 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* reset_to_98- main function
-* @n: integer set
-*
-*/
+ *reset_to_98 - main function
+ *@n: index number
+ */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
+
 }
