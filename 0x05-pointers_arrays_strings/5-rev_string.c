53053fb32c58b70448b587cce38931024325b4b0
hassanzali <hassanzali622@gmail.com>
2023-09-19 04:04:14 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *rev_string - main function to print in reverse
+ *@s: character s
+ *
+ */
+
+void rev_string(char *s)
+{
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	count ++;
+
+for (i = 0; i < count / 2; i++)
+{
+char j;
+	j = s[i];
+	s[i] = s[count - 1 - i];
+	s[count - 1 - i] = j;
+}
+
+}
