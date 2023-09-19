640cce91468be7a198682ebd7eda45edc699d25e
hassanzali <hassanzali622@gmail.com>
2023-09-18 17:59:53 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
 *_strlen - main function
 *@s: function parameter
-*return: cout value
+*Return: cout value 0
 */
 
 int _strlen(char *s)
@@ -11,8 +11,8 @@ int _strlen(char *s)
 	int i;
 	int cout = 0;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0'; i++)
 
-	cout ++;
-	return(cout);
+	cout++;
+	return (cout);
 }
