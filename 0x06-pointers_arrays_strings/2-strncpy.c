ff6431f2fd00e364c993ae4573ae9ba365f1414e
hassanzali <hassanzali622@gmail.com>
2023-09-21 16:36:42 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strncpy - main function
+ *@dest: first parameter
+ *@src: second parameter
+ *@n: third parametet
+ *Return: dest value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int z = 0;
+while (z < n && src[z] != '\0')
+{
+	dest[z] = src[z];
+	z++;
+}
+while (z < n)
+{
+	dest[z] = '\0';
+	z++;
+}
+return (dest);
+}
