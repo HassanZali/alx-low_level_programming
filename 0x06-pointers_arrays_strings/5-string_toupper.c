7ada987885cc64adae2355622ce0cd3fefe6bd2d
hassanzali <hassanzali622@gmail.com>
2023-09-22 08:26:07 -0700

    6-cap

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *string_toupper - function main
+ *@n: string input
+ *@a: parameter one
+ *@z: parameter two
+ *Return: dest
+ */
+
+char *string_toupper(char *)
+{
+int i;
+i = 0;
+
+while (n[i] != '\0')
+
+
+if (n[i] >= 'a' && n[i] <= 'z')
+
+n[i] = n[i] - 32;
+i++;
+
+return (n);
+}
