6b2d6e9cdb7dce5f022ff37cd762cf0fbad8988f
hassanzali <hassanzali622@gmail.com>
2023-09-22 09:20:41 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,13 +13,12 @@ char *string_toupper(char *)
 int i;
 i = 0;
 
-while (n[i] != '\0')
+	while (n[i] != '\0')
+{
+	if (n[i] >= 'a' && n[i] <= 'z')
 
-
-if (n[i] >= 'a' && n[i] <= 'z')
-
-n[i] = n[i] - 32;
-i++;
-
+	n[i] = n[i] - 32;
+	i++;
+}
 return (n);
 }
