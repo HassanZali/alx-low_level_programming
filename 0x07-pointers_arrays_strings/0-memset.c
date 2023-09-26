149d0e32fd3814fecb97d1251c9cc3da03789ac6
hassanzali <hassanzali622@gmail.com>
2023-09-25 18:22:14 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memset - this is the function name
+ *@s: function parameter s
+ *@b: function parameter b
+ *@n: function parameter n
+ *Return s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int z;
+
+	for (z = 0; z < n; z++)
+
+	{
+		s[z] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project provides examples of how to work with pointers,array in c programming language it aim to help new developers learn and understand some fundamental concept
