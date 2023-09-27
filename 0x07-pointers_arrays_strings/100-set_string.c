70866cc5c7b5f1537d1528142efa57ab1d508b68
hassanzali <hassanzali622@gmail.com>
2023-09-26 17:41:08 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_string - this is the  main function name
+ *
+ * @s: function parameter 1
+ *
+ * @to: function parameter 2
+ *
+ * Return: always return 0.
+ */
+
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
