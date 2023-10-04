cc80fdfab0918099b3c959209374af40ff8075dc
hassanzali <hassanzali622@gmail.com>
2023-10-03 19:34:32 -0700

    liball.a

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *atoi - function name
+ *
+ *Return: return 0.
+ */
+int _atoi(void)
+{
+	(void)_atoi;
+
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+this is a readme on how to create a static library how to list it content and how to execute it
